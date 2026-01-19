5364588e5ff80d6d5180379e3f26cac7b0f99d0d
Victoria Cheng <victoria.cheng15@gmail.com>
2026-01-19 10:46:12 -0700

    feat(proxy): add k3s experimental manifests and ADR 007 (#71)

--- proxy/utils/home.go
+++ proxy/utils/home.go
@@ -2,10 +2,32 @@ package utils
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"message": "Welcome to the Observability Hub."})
+}
+
+func DummyHandler(w http.ResponseWriter, r *http.Request) {
+	// 1. Fetch from an external API (GitHub Zen) to test outbound connectivity
+	resp, err := http.Get("https://api.github.com/zen")
+	zenMessage := "Could not fetch Zen"
+	if err == nil {
+		defer resp.Body.Close()
+		body, _ := io.ReadAll(resp.Body)
+		zenMessage = string(body)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": "Hello from k3s proxy_v2!",
+		"outbound_test": map[string]string{
+			"source":  "https://api.github.com/zen",
+			"content": zenMessage,
+		},
+		"status": "experimental",
+	})
 }
